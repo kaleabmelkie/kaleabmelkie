eb6b7db06fafd7367d4c8003e9f08583506dcad3
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2018-11-24 15:04:22 +0300

    Restructured folders and files

---
+++ src/_app/app.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { Default } from '../default'
+
+export class App extends Component {
+  render() {
+    return <Default />
+  }
+}
---
+++ src/default/default.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { Home } from './home'
+
+export class Default extends Component {
+  render() {
+    return <Home />
+  }
+}
