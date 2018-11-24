ba5d73d4913cf439aed10a598132ee41d7c588f5
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2018-11-24 22:44:27 +0300

    Developed the homepage and made many configuration improvements

--- src/default/home/home.tsx
+++ src/default/home/home.tsx
@@ -1,7 +1,132 @@
 import React, { Component } from 'react'
+import './home.scss'
+import kelalTechLogoSrc from '../../_assets/images/kelal-tech-logo.svg'
 
 export class Home extends Component {
   render() {
-    return <div>Kaleab S. Melkie</div>
+    return (
+      <div id="page_home" className="center">
+        <h1>
+          <div id="user-icon">
+            <a href="" title="Refresh this dreamer!">
+              <i className="fa fa-3x fa-user-astronaut" />
+            </a>
+          </div>
+          <div>
+            <span title="Yep! That's my name.">
+              Kaleab <span className="dim">S. Melkie</span>
+            </span>
+          </div>
+          <div className="sub">
+            <span className="dim" title="And this is my title, at The Company.">
+              Co-founder & CEO,{' '}
+            </span>
+            <a
+              href="http://kelaltech.com/"
+              target="_blank"
+              className="kelal-wordmark"
+              title="The Company builds tech that allows smoother information flow."
+            >
+              tech
+            </a>
+          </div>
+        </h1>
+
+        <hr id="below-header" className="size" />
+        <h2 className="left size dim">
+          <span title="Greetings never hurt. Do they?">
+            Nice to Meet You &nbsp; :)
+          </span>
+        </h2>
+
+        <div className="paper size">
+          <h3>
+            <span title="Nice to meet you! Yes, again. Like I just said, greetings never hurt.">
+              YOU CAN FIND ME USING:
+            </span>
+          </h3>
+          <p className="links">
+            <a
+              href="mailto:kaleabmelkie@gmail.com"
+              title="Email this bad boy! (kaleabmelkie@gmail.com)"
+            >
+              <i className="fa fa-3x fa-envelope" />
+              <br />
+              Email
+            </a>
+            <a
+              target="_blank"
+              href="https://linkedin.com/in/kaleabmelkie"
+              title="Connect with Kaleab, the Professional!"
+            >
+              <i className="fab fa-3x fa-linkedin" />
+              <br />
+              LinkedIn
+            </a>
+            <a target="_blank" href="https://t.me/kaleab14" title=";)">
+              <i className="fab fa-3x fa-telegram" />
+              <br />
+              Telegram
+            </a>
+            <a
+              target="_blank"
+              href="https://twitter.com/kaleab14"
+              title="Been here since September 2010."
+            >
+              <i className="fab fa-3x fa-twitter" />
+              <br />
+              Twitter
+            </a>
+          </p>
+
+          <h3>
+            <span title="Dm Dm Dmm Dmmm...">OR, SEE WHAT I DO ON:</span>
+          </h3>
+          <p className="links">
+            <a
+              target="_blank"
+              href="https://www.npmjs.com/~kaleabmelkie"
+              title="I'm an author. Not a book author. Guess what I auth. Authenticate this author. Authorize and Poet-rize. 403 the book author. I'm an author. 200!, for I'm a code author."
+            >
+              <i className="fab fa-3x fa-npm" />
+              <br />
+              NPM
+            </a>
+            <a
+              id="kelalLogo"
+              target="_blank"
+              href="http://kelaltech.com/"
+              title="The Company."
+            >
+              <img src={kelalTechLogoSrc} />
+              <br />
+              kelal tech.
+            </a>
+            <a
+              target="_blank"
+              href="https://gitlab.com/kaleabmelkie"
+              title="Checkout this hard-coder!"
+            >
+              <i className="fab fa-3x fa-gitlab" />
+              <br />
+              GitLab
+            </a>
+            <a
+              target="_blank"
+              href="https://github.com/kaleabmelkie"
+              title="<-- Go GitLab! Boo.. github.  Not much to see here. Really. Much of what I do is on GitLab."
+            >
+              <i className="fab fa-3x fa-github" />
+              <br />
+              GitHub
+            </a>
+          </p>
+        </div>
+
+        <p className="dim" style={{ paddingTop: '3em' }}>
+          <span className="dim">&copy; 2018, Kaleab S. Melkie.</span>
+        </p>
+      </div>
+    )
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
+import initReactFastclick from 'react-fastclick'
 import { App } from './_app'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+initReactFastclick()
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root'),
+)
+
+setTimeout(() => {
+  document.body.style.transition = 'all 3000ms ease-out'
+  document.body.style.opacity = '1'
+  document.body.style.filter = 'opacity(1)'
+  document.body.style.transform = 'scale3d(1,1,1)'
+}, 0)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.register()
