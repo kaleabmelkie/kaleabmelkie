29bbed8d38e902dd01daa28296ba2adc9c86b6c2
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2019-10-30 17:27:33 +0300

    Better WebBox PWA caching options

--- next.config.js
+++ next.config.js
@@ -93,18 +93,11 @@ module.exports = config = withPlugins(
           runtimeCaching: [
             {
               urlPattern: /^https?.*/,
-              handler: 'NetworkFirst',
-              options: {
-                cacheName: 'https-calls',
-                networkTimeoutSeconds: 15,
-                expiration: {
-                  maxEntries: 150,
-                  maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
-                },
-                cacheableResponse: {
-                  statuses: [0, 200]
-                }
-              }
+              handler: 'CacheFirst'
+            },
+            {
+              urlPattern: /(\/service-worker.js$)|(\/api\/)/,
+              handler: 'NetworkFirst'
             }
           ]
         }
