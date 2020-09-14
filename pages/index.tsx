06f386369edd903a272c0a033bb4a72f23b37fc1
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2020-09-14 08:01:37 +0300

    Improve folder structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
-import Home from '../src/components/home/home'
+import LetsConnect from '../src/components/lets-connect/lets-connect'
+import ProfilePhoto from '../src/components/profile-photo/profile-photo'
+import Titles from '../src/components/titles/titles'
 
 export type IndexProps = unknown
 
-const Index: React.FC<IndexProps> = (props) => {
-  return <Home {...props} />
+const Index: React.FC<IndexProps> = () => {
+  return (
+    <>
+      <ProfilePhoto />
+      <Titles />
+      <LetsConnect />
+    </>
+  )
 }
 
 export default Index
