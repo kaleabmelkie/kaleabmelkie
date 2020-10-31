7c16d532a138b4af9123067b2bdb316c6d519ad0
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2020-10-31 15:22:35 +0300

    Add sizes attribute for a truly responsive image

--- src/components/profile-photo/profile-photo.tsx
+++ src/components/profile-photo/profile-photo.tsx
@@ -11,9 +11,11 @@ const ProfilePhoto: React.FC<ProfilePhotoProps> = () => {
         <Image
           alt="Kaleab S. Melkie's Avatar"
           src="https://www.gravatar.com/avatar/2b5daaa0ad27fd75b5d48672e6df074d?size=1080"
-          width={1080}
-          height={1080}
+          width={1440}
+          height={1440}
+          sizes="(min-width: 960px) 33vw, (min-width: 1440px) 25vw, (max-width: 720px) 56vw, (max-width: 480px) 75vw, 38vw"
           priority
+          loading="eager"
         />
       </div>
     </div>
