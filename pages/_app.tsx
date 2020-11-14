e0c962e90c64a1299371361390bccba4d640cb59
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2020-11-14 17:03:41 +0300

    Report real-time Web Vitals to analytics (GTM)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,20 @@ import '../src/assets/styles/global.scss'
 import Footer from '../src/components/footer/footer'
 import seo from '../src/configs/seo'
 
+export async function reportWebVitals({ id, name, label, value }) {
+  // See:
+  // - https://nextjs.org/docs/advanced-features/measuring-performance#sending-results-to-analytics
+  // - https://github.com/GoogleChrome/web-vitals#send-the-results-to-google-tag-manager
+
+  ;(window as any).dataLayer.push({
+    event: label,
+    event_category: label === 'web-vital' ? 'Web Vitals' : `Custom: ${label}`,
+    event_action: name,
+    event_value: Math.round(name === 'CLS' ? value * 1000 : value),
+    event_label: id,
+  })
+}
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
