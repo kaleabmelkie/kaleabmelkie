90749fae35c62996209532a9148c4da71193b3c4
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2023-01-05 13:13:27 +0300

    Fix hanging issue

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -21,102 +21,109 @@ export const load = (async () => {
 }) satisfies PageServerLoad
 
 async function fetchAndCacheItems() {
-	const parse =
-		typeof rssToJson === 'object'
-			? (rssToJson as unknown as { parse: typeof rssToJson }).parse
-			: rssToJson
-
-	let mediumItems: {
-		title: string
-		link: string
-		author: string
-		published: number
-		created: number
-		category: string[]
-		content: string
-		enclosures: string[]
-		content_encoded: string
-		// media: {}
-	}[] = []
 	try {
-		const { items } = await parse('https://medium.com/feed/@kaleabmelkie', {})
-		mediumItems = items
-	} catch {
-		// do nothing
-	}
+		const parse =
+			typeof rssToJson === 'object'
+				? (rssToJson as unknown as { parse: typeof rssToJson }).parse
+				: rssToJson
 
-	let tweetItems: {
-		title: string
-		description: string
-		link: string
-		author: `@${string}`
-		published: number
-		created: number
-		category: string[]
-		enclosures: string[]
-		// media: {}
-	}[] = []
-	try {
-		const { items } = await parse('https://nitter.net/kaleab14/rss', {})
-		tweetItems = items
-	} catch {
-		// do nothing
-	}
+		const [medium, twitter] = await Promise.allSettled([
+			parse('https://medium.com/feed/@kaleabmelkie', {}),
+			parse('https://nitter.net/kaleab14/rss', {}),
+		])
 
-	const items: Item[] = [
-		...mediumItems.slice(0, 10).map((item) => ({
-			published_at: item.published,
-			link: item.link,
-			title: item.title,
-			excerpt_html: truncatise(
-				item.content
-					.replace(/<a /g, `<span `)
-					.replace(/<\/a>/g, `</span>`)
-					.replace(/ href=/g, ` aria-label=`)
+		const mediumItems: {
+			title: string
+			link: string
+			author: string
+			published: number
+			created: number
+			category: string[]
+			content: string
+			enclosures: string[]
+			content_encoded: string
+			// media: {}
+		}[] = medium.status === 'fulfilled' ? medium.value.items : []
+		if (medium.status === 'rejected') {
+			console.error({ mediumError: medium.reason })
+		}
+
+		const twitterItems: {
+			title: string
+			description: string
+			link: string
+			author: `@${string}`
+			published: number
+			created: number
+			category: string[]
+			enclosures: string[]
+			// media: {}
+		}[] = twitter.status === 'fulfilled' ? twitter.value.items : []
+		if (twitter.status === 'rejected') {
+			// log only if it's not an empty feed (nitter returns 404 if the user has no tweets)
+			if (twitter.reason?.response?.status !== 404) {
+				console.error({ twitterError: twitter.reason })
+			}
+		}
+
+		const items: Item[] = [
+			...mediumItems.slice(0, 10).map((item) => ({
+				published_at: item.published,
+				link: item.link,
+				title: item.title,
+				excerpt_html: truncatise(
+					item.content
+						.replace(/<a /g, `<span `)
+						.replace(/<\/a>/g, `</span>`)
+						.replace(/ href=/g, ` aria-label=`)
+						.replace(
+							/<img /g,
+							`<img alt="Article picture of: ${item.title}" height="192px" width="192px" `,
+						)
+						.replace(/\.medium\.com\/max\/[0-9]+\//g, '.medium.com/max/384/')
+						.replace(/ width="[0-9]+"/g, ' width="192"'),
+					{
+						TruncateBy: 'paragraphs',
+						TruncateLength: 2,
+						StripHTML: false,
+						Strict: true,
+						Suffix: `<hr class="hr" /><a class="link" href="${item.link}">Continue reading on Medium »</a>`,
+					},
+				) as string,
+			})),
+
+			...twitterItems.slice(0, 10).map((item) => ({
+				published_at: item.published,
+				link: item.link,
+				title: '{{TWEETER_ICON}}',
+				excerpt_html: `<a class="text-lg md:text-xl cursor-pointer" rel="external" href="${
+					item.link
+				}"><span class="font-normal">${item.description
+					.replace(/<a /g, '<span ')
+					.replace(/<\/a>/g, '</span>')}</span></a>`
 					.replace(
 						/<img /g,
-						`<img alt="Article picture of: ${item.title}" height="192px" width="192px" `,
+						`<img class="mt-4 md:mt-6" alt="Article picture of: ${item.title}" height="192px" width="192px" `,
 					)
-					.replace(/\.medium\.com\/max\/[0-9]+\//g, '.medium.com/max/384/')
-					.replace(/ width="[0-9]+"/g, ' width="192"'),
-				{
-					TruncateBy: 'paragraphs',
-					TruncateLength: 2,
-					StripHTML: false,
-					Strict: true,
-					Suffix: `<hr class="hr" /><a class="link" href="${item.link}">Continue reading on Medium »</a>`,
-				},
-			) as string,
-		})),
-
-		...tweetItems.slice(0, 10).map((item) => ({
-			published_at: item.published,
-			link: item.link,
-			title: '{{TWEETER_ICON}}',
-			excerpt_html: `<a class="text-lg md:text-xl cursor-pointer" rel="external" href="${
-				item.link
-			}"><span class="font-normal">${item.description
-				.replace(/<a /g, '<span ')
-				.replace(/<\/a>/g, '</span>')}</span></a>`
-				.replace(
+					.replace(/ style="max-width:250px;"/g, '')
+					.replace(/nitter\.net/g, 'twitter.com')
+					.replace(/https:\/\/twitter.com\/pic\//g, 'https://nitter.net/pic/'),
+			})),
+		]
+			.sort((a, b) => b.published_at - a.published_at)
+			.map((item, i) => ({
+				...item,
+				excerpt_html: item.excerpt_html.replace(
 					/<img /g,
-					`<img class="mt-4 md:mt-6" alt="Article picture of: ${item.title}" height="192px" width="192px" `,
-				)
-				.replace(/ style="max-width:250px;"/g, '')
-				.replace(/nitter\.net/g, 'twitter.com')
-				.replace(/https:\/\/twitter.com\/pic\//g, 'https://nitter.net/pic/'),
-		})),
-	]
-		.sort((a, b) => b.published_at - a.published_at)
-		.map((item, i) => ({
-			...item,
-			excerpt_html: item.excerpt_html.replace(
-				/<img /g,
-				`<img loading="${i < 1 ? 'eager' : 'lazy'}" `,
-			),
-		}))
+					`<img loading="${i < 1 ? 'eager' : 'lazy'}" `,
+				),
+			}))
 
-	itemsCache.set('default', items)
+		itemsCache.set('default', items)
 
-	return items
+		return items
+	} catch (e) {
+		console.error(e)
+		throw e
+	}
 }
