a8d7f01a9edb117736db8ff09cb99020eb970dce
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2023-02-03 19:01:07 +0300

    Add Partytown + GTM (with GA4) integrations

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
+import { partytownVite } from '@builder.io/partytown/utils'
 import { sveltekit } from '@sveltejs/kit/vite'
+import path from 'path'
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		partytownVite({
+			dest: path.join(__dirname, 'static', '~partytown'),
+		}),
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 	},
